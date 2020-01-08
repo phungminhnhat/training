c67a0aa330768bbacc4736f9210b7942069ea777
phungminhnhat <phungminhnhat1999@gmail.com>
2020-01-08 18:14:13 +0700

    react day3

--- Reactjs/public/submit.js
+++ Reactjs/public/submit.js
@@ -1,130 +1,129 @@
-const {
-    HashRouter,
-    Switch,
-    Route,
-    Link
-  } = ReactRouterDOM
-  
-  // A simple data API that will be used to get the data for our
-  // components. On a real website, a more robust data fetching
-  // solution would be more appropriate.
-  const PlayerAPI = {
-    players: [
-      { number: 1, name: "Ben Blocker", position: "G" },
-      { number: 2, name: "Dave Defender", position: "D" },
-      { number: 3, name: "Sam Sweeper", position: "D" },
-      { number: 4, name: "Matt Midfielder", position: "M" },
-      { number: 5, name: "William Winger", position: "M" },
-      { number: 6, name: "Fillipe Forward", position: "F" }
-    ],
-    all: function() { return this.players},
-    get: function(id) {
-      const isPlayer = p => p.number === id
-      return this.players.find(isPlayer)
-    }
-  }
-  
-  // The FullRoster iterates over all of the players and creates
-  // a link to their profile page.
-  const FullRoster = () => (
-    <div>
-      <ul>
-        {
-          PlayerAPI.all().map(p => (
-            <li key={p.number}>
-              <Link to={`/roster/${p.number}`}>{p.name}</Link>
-            </li>
-          ))
+
+var list; 
+
+class Note extends React.Component{
+    constructor(props) {
+        super(props);
+        this.delete=this.delete.bind(this)
+        this.edit=this.edit.bind(this)
+        this.save=this.save.bind(this)
+        this.cancel=this.cancel.bind(this)
+        this.state={
+            onEdit : false
         }
-      </ul>
-    </div>
-  )
-  
-  // The Player looks up the player using the number parsed from
-  // the URL's pathname. If no player is found with the given
-  // number, then a "player not found" message is displayed.
-  const Player = (props) => {
-    const player = PlayerAPI.get(
-      parseInt(props.match.params.number, 10)
-    )
-    if (!player) {
-      return <div>Sorry, but the player was not found</div>
     }
-    return (
-      <div>
-        <h1>{player.name} (#{player.number})</h1>
-        <h2>Position: {player.position}</h2>
-        <Link to='/roster'>Back</Link>
-      </div>
-    )
-  }
-  
-  // The Roster component matches one of two different routes
-  // depending on the full pathname
-  const Roster = () => (
-    <Switch>
-      <Route exact path='/roster' component={FullRoster}/>
-      <Route path='/roster/:number' component={Player}/>
-    </Switch>
-  )
-  
-  const Schedule = () => (
+    delete(){
+        $.post('/delete',{idXoa : this.props.id},function(data){
+            list.setState({mang:data });
+        });
+    }
+    edit(){
+        this.setState({onEdit : true});
+    }
+    save(){
+        var note=this;
+        $.post('/update',{idSua:this.props.id,noidung:this.refs.txt.value},function(data){
+            list.setState({mang: data});
+            note.setState({onEdit:false});
+        });
+    }
+    cancel(){
+        this.setState({onEdit:false});
+    }
+render() {
+    if(this.state.onEdit){
+        return(
+            <div className="note">
+             <input ref="txt" defaultValue={this.props.children} />
+                <button className="button" onClick={this.save}>Lưu</button>
+                <button className="button" onClick={this.cancel}>Hủy</button>
+            </div>
+        )
+    }else{
+        return (
+            <div className="note">
+             <p>{this.props.children}</p>
+                <button className="button" onClick={this.delete}>Xóa</button>
+                <button className="button" onClick={this.edit}>Sửa</button>
+            </div>
+        );
+    }
+    
+}
+};
+class List extends React.Component{
+    constructor(props) {
+        super(props);
+        list=this
+        this.add=this.add.bind(this)
+        this.getState=this.getState.bind(this)
+        this.state={mang:[]}
+    }
+    
+    getState(){
+        
+        this.setState({mang : this.state.mang});
+    }
+    add(){
+        ReactDOM.render(
+            <InputText></InputText>
+            ,
+            document.getElementById('add-div')
+        )
+    }
+    render() {
+        return (
+            <div className="list">
+                <button onClick={this.add}>Thêm</button>
+                <div id="add-div"></div>
+                {
+                    this.state.mang.map(function(note,index){
+                        return <Note key={index} id={index}>{note}</Note>
+                        
+                    })
+                }
+                
+            </div>
+        );
+    };
+
+    componentDidMount(){
+        $.post('/getNote',function(data){
+            list.setState({mang : data});
+        });
+    };
+
+    };
+
+class InputText extends React.Component{
+    constructor(props) {
+        super(props);
+        
+        this.sendNote=this.sendNote.bind(this)
+    }
+    sendNote(){
+        $.post("/add",{note : this.refs.txt.value},function(data){
+            list.setState({mang : data})
+        });
+
+        ReactDOM.unmountComponentAtNode(document.getElementById('add-div'))
+    }
+    render() {
+        return (
+            <div>
+                <input type="text" ref="txt" placeholder="Ghi note vào đây !!"></input>
+                <button onClick={this.sendNote}>Gửi Note</button>
+            </div>
+        );
+    }
+}
+
+
+ReactDOM.render(   
     <div>
-      <ul>
-        <li>6/5 @ Evergreens</li>
-        <li>6/8 vs Kickers</li>
-        <li>6/14 @ United</li>
-      </ul>
+        
+        <List></List>
     </div>
-  )
-  
-  const Home = () => (
-    <div>
-      <h1>Welcome to the Tornadoes Website!</h1>
-    </div>
-  )
-  
-  // The Main component renders one of the three provided
-  // Routes (provided that one matches). Both the /roster
-  // and /schedule routes will match any pathname that starts
-  // with /roster or /schedule. The / route will only match
-  // when the pathname is exactly the string "/"
-  const Main = () => (
-    <main>
-      <Switch>
-        <Route exact path='/' component={Home}/>
-        <Route path='/roster' component={Roster}/>
-        <Route path='/schedule' component={Schedule}/>
-      </Switch>
-    </main>
-  )
-  
-  // The Header creates links that can be used to navigate
-  // between routes.
-  const Header = () => (
-    <header>
-      <nav>
-        <ul>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/roster'>Roster</Link></li>
-          <li><Link to='/schedule'>Schedule</Link></li>
-        </ul>
-      </nav>
-    </header>
-  )
-  
-  const App = () => (
-    <div>
-      <Header />
-      <Main />
-    </div>
-  )
-  
-  // This demo uses a HashRouter instead of BrowserRouter
-  // because there is no server to match URLs
-  ReactDOM.render((
-    <HashRouter>
-      <App />
-    </HashRouter>
-  ), document.getElementById('root'))
-  
+, 
+document.getElementById("root")
+);
